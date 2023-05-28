78639e48c909f6c31cd8db700de985ee746130fb
Maddimsetti Ravi Teja <raviteja985@gmail.com>
2023-05-28 20:48:58 +0530

    first commit

---
+++ src/components/Greetings.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function Greetings(){
+//     return <h1>Welcome to React Learning</h1>
+// }
+
+const Greetings =()=><h1>Welcome to Learn React</h1>
+
+export default Greetings 
